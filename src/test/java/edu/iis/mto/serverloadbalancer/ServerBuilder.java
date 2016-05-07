04c0bf8404b42be9c632045cfb86bfef3ffd3029
msut <marr994@o2.pl>
2016-05-07 21:17:42 +0200

    add server builder

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by mariusz on 07.05.16.
+ */
+public class ServerBuilder {
+
+    private int capacity;
+
+    public Server build() {
+        return new Server();
+    }
+
+    public ServerBuilder withCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
