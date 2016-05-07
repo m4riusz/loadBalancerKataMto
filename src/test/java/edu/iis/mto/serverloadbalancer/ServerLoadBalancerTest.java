4ab4ad52ae78c772c1bff4fb8007a39d35c1db33
msut <marr994@o2.pl>
2016-05-07 21:16:51 +0200

    create test

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,16 +1,23 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import org.junit.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.Test;
-
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
 
+    @Test
+    public void balancingServerWithNoVms_serverStaysEmpty() {
+        Server server = a(server().withCapacity(1));
+        balancing(aServerWith(server), anEmptyListOfVms());
+
+        assertThat(server, hasCurrentLoadOf(0.0d));
+    }
 
 }
