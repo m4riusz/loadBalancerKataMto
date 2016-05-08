c64434bb9b7226ecc02e29185c64b730dd63b426
msut <marr994@o2.pl>
2016-05-08 17:48:19 +0200

    add matcher

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,4 +21,27 @@ public class ServerLoadBalancerTest {
         assertThat(server, hasCurrentLoadOf(0.0d));
     }
 
+    private Matcher<? super Server> hasCurrentLoadOf(double expectedLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+    }
+
+    private void balancing(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers,vms);
+    }
+
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
+
+    private Server[] aServerWith(Server... servers) {
+        return servers;
+    }
+
+    public Server a(ServerBuilder serverBuilder){
+        return serverBuilder.build();
+    }
+
+    private ServerBuilder server() {
+        return new ServerBuilder();
+    }
 }
