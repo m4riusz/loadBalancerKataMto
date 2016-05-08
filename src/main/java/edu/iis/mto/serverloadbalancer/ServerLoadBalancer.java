35782926a94e39b099e323212cfc71c24fef5373
msut <marr994@o2.pl>
2016-05-08 17:41:47 +0200

    add server load balancer

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -0,0 +1,10 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by mariusz on 07.05.16.
+ */
+public class ServerLoadBalancer {
+    public void balance(Server[] servers, Vm[] vms) {
+
+    }
+}
