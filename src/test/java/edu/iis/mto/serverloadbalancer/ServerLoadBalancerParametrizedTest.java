3b4a6270bbba769be569e86f4c30b43aecdcc697
msut <marr994@o2.pl>
2016-05-23 22:00:08 +0200

    add parametrized test

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -1,22 +1,46 @@
 package edu.iis.mto.serverloadbalancer;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.runners.Parameterized.Parameters;
 
-import org.junit.Test;
+@RunWith(Parameterized.class)
+public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest {
 
-public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
-	@Test
-	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+    private int serverCapacity;
+    private int vmSize;
+    private double percentageLoad;
 
-		assertThat(theServer, hasLoadPercentageOf(100.0d));
-		assertThat("the server should contain vm", theServer.contains(theVm));
-	}
-	
-	
+    public ServerLoadBalancerParametrizedTest(int serverCapacity, int vmSize, double percentageLoad) {
+        this.serverCapacity = serverCapacity;
+        this.vmSize = vmSize;
+        this.percentageLoad = percentageLoad;
+    }
+
+    @Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {10, 1, 10.0d}, {4, 2, 50.0d}, {5, 2, 40.0d}, {7, 3, 42.85d}
+        });
+    }
+
+    @Test
+    public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
+        Server theServer = a(server().withCapacity(serverCapacity));
+        Vm theVm = a(vm().ofSize(vmSize));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat(theServer, hasLoadPercentageOf(percentageLoad));
+        assertThat("the server should contain vm", theServer.contains(theVm));
+    }
+
+
 }
